c4780237d2e5fed2480a1e4632143a50a2ff871c
Kyu3109 <141044715+Kyu3109@users.noreply.github.com>
2023-09-26 09:06:20 -0300

    Organizador
    100%funcional mesmo que ainda esteja tendo um "erro"
    Move somente arquivos [png, jpg, jpeg, zip, apk, mp4]

---
+++ organizador.py
@@ -0,0 +1,76 @@
+import os
+
+# Verifica se tem a pasta.
+Destiny = '/storage/emulated/0/Download/mídias'
+
+# Variavel para caso não houver nenhum arquivo para mover.
+nada = "Nᴀ̃ᴏ ᴛᴇᴍ ɴᴇɴʜᴜᴍ ᴀʀǫᴜɪᴠᴏ ᴘᴀʀᴀ ᴍᴏᴠᴇʀ :3"
+
+if os.path.exists(Destiny):
+   print(" _  __")
+   print("| |/ /")
+   print("| ' /  _   _  _   _")
+   print("|  <  | | | || | | |")
+   print("| . \ | |_| || |_| |")
+   print("|_|\_\ \__, | \__,_|")
+   print("        __/ |")
+   print("       |___/")
+
+
+# Cria pastas caso não houver pastas criadas
+else:
+    os.system('mkdir mídias &&  cd mídias && mkdir imagens && mkdir vídeos && mkdir zip && mkdir apk')
+    print("as pastas foram criadas com sucessi")
+
+print("-"*30)
+
+# Pasta principal.
+list = '/storage/emulated/0/Download'
+
+# Guarda os arquivos.
+files_list = [os.path.join(list, files)
+
+# Lista os arquivos.
+for files in os.listdir(list)
+    if files.endswith(('.jpg', '.zip', '.jpeg', '.png', '.apk', '.mp4'))]
+
+# Move arquivos de imagens png/jpg/jpeg.
+for files in files_list:
+    if files.endswith('.png'):
+       print("𝐌𝐨𝐯𝐞𝐧𝐝𝐨 𝐚𝐫𝐪𝐮𝐢𝐯𝐨𝐬 𝐩𝐧𝐠")
+       os.system("mv *.png /storage/emulated/0/Download/mídias/imagens")
+
+for files in files_list:
+    if files.endswith('.jpg'):
+       print("𝐌𝐨𝐯𝐞𝐧𝐝𝐨 𝐚𝐫𝐪𝐮𝐢𝐯𝐨𝐬 𝐣𝐩𝐠")
+       os.system("mv *.jpg /storage/emulated/0/Download/mídias/imagens")
+
+for files in files_list:
+    if files.endswith('.jpeg'):
+       print("𝐌𝐨𝐯𝐞𝐧𝐝𝐨 𝐚𝐫𝐪𝐮𝐢𝐯𝐨𝐬 𝐣𝐩𝐞𝐠")
+       os.system("mv *.jpeg /storage/emulated/0/Download/mídias/imagens")
+
+# Move arquivos zip.
+for files in files_list:
+    if files.endswith('.zip'):
+       print("𝐌𝐨𝐯𝐞𝐧𝐝𝐨 𝐚𝐫𝐪𝐮𝐢𝐯𝐨𝐬 𝐳𝐢𝐩")
+       os.system("mv *.zip /storage/emulated/0/Download/mídias/zip")
+
+# Move arquivos de vídeos mp4.
+for files in files_list:
+    if files.endswith('.mp4'):
+       print("𝐌𝐨𝐯𝐞𝐧𝐝𝐨 𝐚𝐫𝐪𝐮𝐢𝐯𝐨𝐬 𝐦𝐩4")
+       os.system("mv *.mp4 /storage/emulated/0/Download/mídias/vídeos")
+
+# Move arquivos de aplicações apk.
+for files in files_list:
+    if files.endswith('.apk'):
+       print("𝐌𝐨𝐯𝐞𝐧𝐝𝐨 𝐚𝐫𝐪𝐮𝐢𝐯𝐨𝐬 𝐚𝐩𝐤")
+       os.system("mv *.apk /storage/emulated/0/Download/mídias/apk")
+
+# Caso a tarefa seja concluída ou não tenha nenhum arquivo para mover.
+if files_list:
+   print("tarefa concluída")
+
+else:
+    print(nada)
